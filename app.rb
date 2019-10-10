b2ff5c18a2b38bb402f0b0560c8216721fce3685
Neal JIn Oh <no4259@gmail.com>
2019-10-10 20:11:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,13 +8,29 @@ class App < Sinatra::Base
   end
 
   get '/square/:number' do
-    @num1 = params[:number]
-    @num2 = params[:number]
+    @num1 = params[:number].to_i**2
 
-    @num1**@num2
+    @num1.to_s
   end
 
   get '/say/:number/:phrase' do
   end
 
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
+  end
+  
+  get '/:operation/:number1/:number2' do
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+    
+    (@num1+@num2).to_s
+    (@num2-@num1).to_s
+  end
+  
 end
