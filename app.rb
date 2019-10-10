cd486698f19f6b7b562797ca1d7709dd8d3a3a42
Neal JIn Oh <no4259@gmail.com>
2019-10-10 20:12:59 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -26,6 +26,7 @@ class App < Sinatra::Base
   end
   
   get '/:operation/:number1/:number2' do
+    @op = params[:operation]
     @num1 = params[:number1].to_i
     @num2 = params[:number2].to_i
     
